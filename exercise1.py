6be799c54076aff19ac646e41a5bd39bf1fa2577
Hilario II "Lars" B. Arriesgado <118493724+1Larsce@users.noreply.github.com>
2022-11-18 10:21:50 +0800

    Add files via upload

---
+++ exercise1.py
@@ -0,0 +1,10 @@
+name = input("Enter username: ")
+age = input("Enter age: ")
+first_number = input("Enter the first number: ")
+second_number = input("Enter the second number: ")
+
+print("The username is " + name + " and the age is " + str(age))
+
+product = int(first_number) * int(second_number)
+
+print("The product is " + str(product))
