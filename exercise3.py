c1d65d77611297da8b4d68668f38ba160d0fbee3
Hilario II "Lars" B. Arriesgado <118493724+1Larsce@users.noreply.github.com>
2022-11-24 23:33:17 +0800

    Add files via upload

---
+++ exercise3.py
@@ -0,0 +1,5 @@
+aYear = int(365) #number of days in a year
+aDay = int(24) #number of hours in a day
+anHour = int(60) #number of minutes in an hour
+
+print("The number of minutes in a year is ", (anHour * aDay) * aYear)
---
+++ exercise4.py
@@ -0,0 +1,6 @@
+rate = int(3 * 100000000)
+seconds = int(365 * 24 * 60 * 60)
+distance = int(rate * seconds)
+
+print("Light travels", distance, "meters in a year.")
+
